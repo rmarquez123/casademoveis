cdaa4e8da265a40bb175fdce28781c2993d7abe9
Ricardo Marquez <ricardo.marquez@pitchaero.com>
2025-11-19 09:47:20 -0300

    updates.

--- src/main/java/used_furniture/restapi/UsedFurnitureConfiguration.java
+++ src/main/java/used_furniture/restapi/UsedFurnitureConfiguration.java
@@ -4,7 +4,9 @@ import com.zaxxer.hikari.HikariConfig;
 import common.db.ConnectionPool;
 import common.db.DbConnection;
 import common.db.HikariConnectionPool;
+import java.util.Properties;
 import java.util.function.Consumer;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,15 +18,16 @@ import org.springframework.context.annotation.Configuration;
 public class UsedFurnitureConfiguration {
   
   @Bean("used_furniture.conn")
-  public DbConnection dbconn() {
-    String url = "localhost";  
-    int port = 5433;
-    String databaseName = "casademoveis";
-    String user = "postgres"; 
-    String password = "postgres"; 
+  public DbConnection dbconn(@Qualifier("appProps") Properties appProps) {
+
+    String url = appProps.getProperty("used-furniture.db.url");
+    int port = Integer.parseInt(appProps.getProperty("used-furniture.db.port"));
+    String databaseName = appProps.getProperty("used-furniture.db.name");
+    String user = appProps.getProperty("used-furniture.db.user");
+    String password = appProps.getProperty("used-furniture.db.password"); 
     Consumer<HikariConfig> configHelper = p -> {
     };
     ConnectionPool connPool = new HikariConnectionPool(url, port, databaseName, user, password, configHelper);
-    return new DbConnection(connPool);  
+    return new DbConnection(connPool);
   }
 }
